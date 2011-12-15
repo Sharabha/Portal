b09e230e68a3b96da5943a61215a90bdc8b9d205
Adam Kijak <adamkijak@o2.pl>
2011-12-15 20:56:40 +0100

    Fix: syntax fixed

--- app/models/user_team_membership.rb
+++ app/models/user_team_membership.rb
@@ -2,20 +2,20 @@ class UserTeamMembership < ActiveRecord::Base
 
   belongs_to :team
   belongs_to :user
-validate :ensure_is_space_in_team, :before => :create
+
+  validate :ensure_is_space_in_team, :before => :create
+  validates_presence_of :team_id
+  validates_presence_of :user_id
+
   private
   def ensure_is_space_in_team
-  id = TeamMembership.find(self.team_id).competition_id
-  max_users_property = Competition.find(id).max_users
-    if( Team.find(self.id).count >=max_users_property )
+    id = TeamMembership.find(self.team_id).competition_id
+    max_users_property = Competition.find(id).max_users
+    if Team.find(self.id).count >= max_users_property
 	   return false
 	   else return true
     end
   end
-end
 
-  validates_presence_of :team_id
-  validates_presence_of :user_id
 end
-
 
