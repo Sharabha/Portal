9557ebc94c5b131d8b4c97cb7a727a609635d0c0
uzytkownik <uzytkownik@lays.ru>
2011-12-18 21:20:29 +0100

    Dodanie pola login do usera

--- db/schema.rb
+++ db/schema.rb
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(:version => 20111216221122) do
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "",    :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
+    t.string   "login"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
