7837aeebc6c08213ed76873089387c878a2356bf
Marcin Wierzbicki <cinkowskiw@gmail.com>
2011-12-27 14:05:38 +0100

    drobna poprawka (testy nie przechodziły)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -69,14 +69,14 @@ class Ability
 
         #przegląda i wysyła rozwiązania z zawodów w których uczestniczy
         can :manage, Solution do
-            puts "\n\n\n\n\n\n************************* Test1"
+          #puts "\n\n\n\n\n\n************************* Test1"
           |solution| solution.teams.any?{|t|
             t.leader_id==user.id
           }
         end
 
         can :manage, Solution do
-          puts "\n\n\n\n\n\n\n\nxxxxxxxxxxxxxxxxxxxxxxxxx Test2"
+          #puts "\n\n\n\n\n\n\n\nxxxxxxxxxxxxxxxxxxxxxxxxx Test2"
           |solution| solution.team_members.any?{|u|
             u.id == user.id
           }
