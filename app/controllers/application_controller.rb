a7dfcbf26cfbbaa899f328c9e01104d156ad712e
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-01-16 08:59:51 +0100

    notice fix & locale parsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+ 
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    unless parsed_locale.blank?
+      I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+    end
+  end
 
   rescue_from CanCan::AccessDenied do |exception|  
     flash[:error] = "Nie masz prawa do wykonania tej akcji!"  
     redirect_to root_url  
   end  
+
 end
