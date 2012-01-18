179504dc6e4618c7dea4ba104e2607222a55f3bd
Paweł Kalinowski <anilatx@gmail.com>
2012-01-18 15:33:08 +0100

    Prowizoryczna (ale dzialajaca) sprawdzaczka, uwaga, ocenianie odpalane recznie(np via ssh)

--- app/models/checker.rb
+++ app/models/checker.rb
@@ -5,13 +5,17 @@ class Checker < ActiveRecord::Base
 
   def check_solution(solution)
     score = 0
-    for cd in self.checker_datas
-      score += self.check_one(solution, cd)
-    end
+    test_id=0
+    wynik = File.open("checker/result/"+problem.id.to_s+"-"+test_id.to_s+".txt", "rb") {|io| io.read}
+    puts "Ocenia:"+problem.id.to_s+" na:"+wynik
+    return wynik.to_i
+    #for cd in self.checker_datas
+      #score += self.check_one(solution, cd)
+    #end
     #returns % of passed tests
-    len = self.checker_datas.length
-    return 0 unless len > 0
-    score / len
+    #len = self.checker_datas.length
+    #return 0 unless len > 0
+    #score / len
   end
 
   def check_one(solution, checker_data)
