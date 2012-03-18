2ab7e6461dfb89e0dea20dd0a443a9ec1a4c20c2
Ostrzy <uslugi@ostrzy.net>
2012-03-18 23:12:23 +0100

    Abilities rewritten

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,133 +1,51 @@
 class Ability
   include CanCan::Ability
 
-  #TODO: teams
-  #TODO: user_team_membership
-
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user.has_role?(:admin)
         can :manage, :all
         can :make_admin, :user
         can :remove_admin, :user
-    elsif JudgeMembership.any?{|x| user.id==x.judge_id}
-        #SEDZIA
-        # moze czytac wszystkie zadania
+    elsif user.has_role?(:organizer)
+        #ORGANIZATOR
+        can :manage, Competition, :organizer_id => user.id
+
+        can :manage, JudgeMembership, :competition => { :organizer_id => user.id }
+
         can :read, Problem
 
-        # zawodów może podpiąć zadanie do zawodów którym sędziuje
-        can :create, ProblemMembership do |problem|
-          problem.competition.judges.any?{|x| user.id==x.id}
-        end
+        can :manage, TeamMembership, :competition => { :organizer_id => user.id }
 
-        #sedzia czyta zawody którym sędziuje
-        can :read, Competition do |comp|
-          comp.judge_ids.include? user.id
-        end
+        can :delete, ProblemMembership, :competition => { :organizer_id => user.id }
+
+        can :manage, Solution, :competition => { :organizer_id => user.id }
+    elsif user.has_role?(:guardian)
+        #SEDZIA
+        can :read, Problem
+
+        can :create, ProblemMembership, :competition => { :judges => { :id => user.id } }
+
+        can :read, Competition, :judges => { :id => user.id }
 
         #sedzia czyta tulko zadania z zawodów
-        can :read, ProblemMembership do |problem_membership|
-          problem_membership.problem.competition.judges.any?{|x| user.id==x.id}
-        end
-
-        #TODO sędzia przegląda tylko zespoły zapisane w zawodach
-        #can :read, Team do |team|
-        #  team.competition.judge.any?{|x| user.id == x.id}
-        #end
-
-        
-        # INFO: sędzia nie musi się zajmować członkostwami w zespole
-        # -- od tego jest przywódca zespołu
-
-    elsif Competition.any? {|x| user.id==x.organizer_id}
-        #ORGANIZATOR
-        #zarządza swoimi zawodami
-        can :manage, Competition, :organizer_id=>user.id
-
-        # może podpiąć sędziów do swoich zawodów
-        can :manage, JudgeMembership do |judge_membership|
-          judge_membership.competition.organizer_id == user.id
-        end
-
-        # organizator moze czytac wszystkie zadania
-        if Competition.any?{|x| user.id==x.organizer_id}
-            can :read, Problem
-        end
-
-        # organizator zarzadza druzynami w swoich zawodach
-        can :manage, TeamMembership do |team_membership|
-          team_membership.competition.organizer_id == user.id
-        end
-
-        # tylko organizator moze usuwac zadania
-        can :delete, ProblemMembership do |problem|
-          problem.competition.organizer_id == user.id
-        end
-
-        #TODO organizator zarządza zespołami zapisanymi na zawody
-        #can :manage, Team do |team|
-        # team.competition.organizer_id == user.id
-        #end
-        
-        #zarządza rozwiązaniami z zawodów w których uczestniczy
-        can :manage, Solution do |solution|
-          solution.competition.organizer_id == user.id
-        end
-
-        # INFO: organizator nie musi się zajmować członkostwami w zespole
-        # -- od tego jest przywódca zespołu
-
+        can :read, ProblemMembership, :problem => { :competition => { :judges => { :id => user.id } } }
     else
         #ZAWODNIK
-        #przegląda zawody w których uczestniczy
-        can :read, Competition do |comp|
-          comp.teams.any?{|t| t.leader_id==user.id}
-        end
+        can :read, Competition, :teams => { :leader_id => user.id }
+        can :read, Competition, :team_members => { :id => user.id }
 
-        can :read, Competition do |comp|
-          comp.team_members.any?{|u| u.id == user.id}
-        end
-        
-        #przegląda zadania w zawodach w których uczestniczy
-        can :read, ProblemMembership do |problem_membership|
-          problem_membership.competition.teams.any?{|t| t.leader_id==user.id}
-        end
+        can :read, ProblemMembership, :competition => { :teams => { :leader_id => user.id } }
+        can :read, ProblemMembership, :competition => { :team_members => { :id => user.id } }
 
-        can :read, ProblemMembership do |problem_membership|
-          problem_membership.competition.team_members.any?{|u| u.id == user.id}
-        end
+        can :manage, Solution, :team => { :leader_id => user.id }
+        can :manage, Solution, :team_members => { :id => user.id }
 
-        #zarządza rozwiązaniami z zawodów w których uczestniczy
-        can :manage, Solution do |solution|
-          solution.team.leader_id == user.id
-        end
+        can :manage, Team, :leader_id => user.id
+        can :read, Team, :team_members => { :id => user.id }
 
-        can :manage, Solution do |solution|
-          solution.team_members.any?{|u| u.id == user.id }
-        end
-
-        #zakladanie zespolu
-        can :create, Team
-
-        #zawodnik czyta nazwę zespołu
-        can :read, Team do |team|
-          team.team_members.any?{|u| u.id == user.id}
-        end
-        
-        #leader zarządza zespołem
-        can :manage, Team do |team|
-          team.leader_id == user.id
-        end
-
-        #zawodnik czyta członkostwa zespołu
-        can :read, UserTeamMembership do |user_team_m|
-          user_team_m.team_team.team_members.any?{|u| u.id == user.id}
-        end
-
-        #leader zarządza członkostwami zespołu
-        can :manage, UserTeamMembership do |user_team_m|
-          user_team_m.team.leader_id == user.id
-        end
+        can :read, UserTeamMembership, :team => { :team_members => { :id => user.id } }
+        can :read, UserTeamMembership, :team => { :leader_id => user.id }
 
         #NOBODY
         can :index, Competition
