c87dc2d086fdcc69c0913a9b9a3ec3d212cb77c6
Lukasz Zatorski <lzatorski@gmail.com>
2012-04-24 17:08:54 +0200

    Removed wrong files from migrations

--- db/migrate/20111205153851_create_checkers.rb
+++
@@ -1,8 +0,0 @@
-class CreateCheckers < ActiveRecord::Migration
-  def change
-    create_table :checkers do |t|
-      t.integer       :problem_id
-      t.timestamps
-    end
-  end
-end
--- db/migrate/add_login_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddLoginToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :login, :string, :unique => true
-  end
-end
