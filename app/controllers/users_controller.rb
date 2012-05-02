97906125215bb7fff277bb2c26b6429ffc2821c0
Ostrzy <uslugi@ostrzy.net>
2012-05-02 18:07:49 +0200

    Remove users from user space

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  before_filter :authenticate_user!
-
-  def index
-    @administrators = Role.where(:name => 'admin').first.users
-    @users = User.all
-  end
-end
