224a569980a0c035476120b73dc4031ba524b08a
Lukasz Zatorski <lzatorski@gmail.com>
2012-05-06 00:45:37 +0200

    Datetime picker addded for competitions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,8 @@ module ApplicationHelper
  def competition_name(competition)
     name = competition.name
  end
+ def date_for_form(date)
+    return date.nil? ? '' : date.strftime('%Y-%m-%d %H:%M')
+ end
 
 end
