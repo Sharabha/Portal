4eff3fffae907e8bb3d984bfbcd359a7e0782815
Michal Banasiak <m.banasiak@aol.pl>
2012-05-07 01:16:02 +0200

    added model test

---
+++ spec/factories/tests.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :test do
+      name "MyString"
+      max_memory 1
+      file ""
+    end
+end
