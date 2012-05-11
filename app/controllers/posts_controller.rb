ca83fc52ff9bc26b617cf2d95c3c59c3cf89e9cb
Maciej Jaromin <maciej_jaromin@o2.pl>
2012-05-11 22:19:34 +0200

    Basic controllers, placeholder new and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < InheritedResources::Base
+  def index
+	
+  end
+
+  def show
+	@post = Post.find(params[:id])
+  end
+
+  def new
+	@post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+	@post.user_id = current_user.id
+    if @post.save
+      redirect_to @post 
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+	@post = Post.find(params[:id])
+  end
+
+  def update
+	@post = Post.find(params[:id])
+  end
+
+  def destroy
+	@post = Post.find(params[:id])
+  end
+
 end
