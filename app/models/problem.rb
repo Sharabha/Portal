7a46111999eead7d4524bfe41507c1eb2283065c
Lukasz Zatorski <lzatorski@gmail.com>
2012-05-27 23:56:40 +0200

    From now on you cannot remove problem when there are any problem memberships

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -10,8 +10,8 @@ class Problem < ActiveRecord::Base
   has_many :guardian_memberships
   has_many :guardians, :through => :guardian_memberships
 
-  has_many :problem_memerships
-  has_many :competitions, :through => :problem_memerships
+  has_many :problem_memberships
+  has_many :competitions, :through => :problem_memberships, :dependent => :restrict
 
   accepts_nested_attributes_for :guardians
 
@@ -23,7 +23,7 @@ class Problem < ActiveRecord::Base
   after_create :notify_checker
 
   attr_protected :author_id
-
+  
   private
     def notify_checker
 	  #to be implemented; this function should ask checker for creation of new problem
