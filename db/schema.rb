6a65a013932704eba12ac4790b34884e4d913c1b
Lukasz Zatorski <lzatorski@gmail.com>
2012-05-30 18:38:25 +0200

    DB schema reloaded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120527195221) do
 
   create_table "competitions", :force => true do |t|
